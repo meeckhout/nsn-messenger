4eac755bfe098d4eb6985edf06c242dbb7780f06
meeckhout <marthe.eeckhout@gmail.com>
2022-09-12 11:29:58 +0200

    Test

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# nsn-messenger
-Nostalgia Network Messenger
--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import {Login, Dashboard, Chatbox, Register, About, Landing_Page} from './pages'
 function App() {
     return (
         <div className="App">
-            <h1>Welcome to React Router!</h1>
             <BreakpointProvider>
                 <Router>
                     <Routes>
                         <Route path="/" element={<Landing_Page />} />
                         <Route path="about" element={<About />} />
-                        <Route path="/Login" element={<Login />} />
-                        <Route path="/Register" element={<Register />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
                         <Route path="/chatbox" element={<Chatbox />} />
                         <Route path="/dashboard" element={<Dashboard />} />
                     </Routes>
