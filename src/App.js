3b11dd183e47a5761faa75e785aa202155c1c2ea
Giovanni Overmeire <101796485+Gioke@users.noreply.github.com>
2022-09-30 11:35:21 +0200

    Add app.js hooks(map) and useScript.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { BreakpointProvider } from 'react-socks';
 import {Login, Dashboard, Chatbox, Register, About, Landing_Page} from './pages';
-
+import Bejeweled from "./pages/Bejeweled";
 
 
 function App() {
@@ -11,12 +11,13 @@ function App() {
             <BreakpointProvider>
                 <Router>
                     <Routes>
-                        <Route path="/" element={<Landing_Page />} />
-                        <Route path="About" element={<About />} />
-                        <Route path="/Login" element={<Login />} />
-                        <Route path="/Register" element={<Register />} />
-                        <Route path="/Chatbox" element={<Chatbox />} />
-                        <Route path="/Dashboard" element={<Dashboard />} />
+                        <Route path="/" element={< Landing_Page />} />
+                        <Route path="About" element={< About />} />
+                        <Route path="/Login" element={< Login />} />
+                        <Route path="/Register" element={< Register />} />
+                        <Route path="/Chatbox" element={< Chatbox />} />
+                        <Route path="/Dashboard" element={< Dashboard />} />
+                        <Route path="/Bejeweled" element={< Bejeweled />} />
                     </Routes>
                 </Router>
             </BreakpointProvider>
